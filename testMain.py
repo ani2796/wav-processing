856195c30e3970aadcad35c4a8efad96dff6d1a1
ani2796 <anirudh1996@hotmail.com>
2016-06-20 19:06:30 +0530

    Modularized the code and reduced it to one function call to wavProcessing.py

--- testMain.py
+++ testMain.py
@@ -8,30 +8,4 @@ import numpy as np
 import pyfftw
 import matplotlib.pyplot as plt
 
-pyfftw.interfaces.cache.enable()
-
-audioFiles = []
-
-sampleAudioDirectory = './Sample Audio/'
-
-audioFiles = wP.get_audio_from_dir(sampleAudioDirectory)
-
-print("The WAV files in the given folder are:\n")
-print(audioFiles, "\n\n")
-
-
-(inputData, outputData) = wP.fft_and_blocks_and_chunks(audioFiles, sampleAudioDirectory)
-
-mean = np.mean(np.mean(inputData, axis=0), axis=0) #Mean across num examples and num timesteps
-std = np.sqrt(np.mean(np.mean(np.abs(inputData-mean)**2, axis=0), axis=0)) # STD across num examples and num timesteps
-std = np.maximum(1.0e-8, std) #Clamp variance if too tiny
-inputData[:][:] -= mean #Mean 0
-inputData[:][:] /= std #Variance 1
-outputData[:][:] -= mean #Mean 0
-outputData[:][:] /= std #Variance 1
-
-np.save('YourMusicLibrary' +'_mean', mean)
-np.save('YourMusicLibrary' +'_var', std)
-np.save('YourMusicLibrary' +'_x', inputData)
-np.save('YourMusicLibrary' +'_y', outputData)
-print("Done!")
+wP.process_audio_for_rnn()
--- wavProcessing.py
+++ wavProcessing.py
@@ -9,6 +9,8 @@ import numpy as np
 import math
 import pyfftw
 
+pyfftw.interfaces.cache.enable()
+
 
 # The below method reads the data within a WAV file and returns a list of the data elements
 def read_wav_file(fileName, directory) :
@@ -173,3 +175,30 @@ def fft_and_blocks_and_chunks(audioFiles, sampleAudioDirectory) :
 	print("No. of chunks:", len(chunksInput), " ", len(chunksInput[0]), " ", len(chunksInput[0][0]))	
 
 	return chunksInput, chunksOutput
+
+
+def process_audio_for_rnn() :
+
+	audioFiles = []
+	sampleAudioDirectory = './Sample Audio/'
+	
+	audioFiles = get_audio_from_dir(sampleAudioDirectory)
+
+	print("The WAV files in the given folder are:\n")
+	print(audioFiles, "\n\n")
+
+	(inputData, outputData) = fft_and_blocks_and_chunks(audioFiles, sampleAudioDirectory)
+
+	mean = np.mean(np.mean(inputData, axis=0), axis=0) #Mean across num examples and num timesteps
+	std = np.sqrt(np.mean(np.mean(np.abs(inputData-mean)**2, axis=0), axis=0)) # STD across num examples and num timesteps
+	std = np.maximum(1.0e-8, std) #Clamp variance if too tiny
+	inputData[:][:] -= mean #Mean 0
+	inputData[:][:] /= std #Variance 1
+	outputData[:][:] -= mean #Mean 0
+	outputData[:][:] /= std #Variance 1
+
+	np.save('YourMusicLibrary' +'_mean', mean)
+	np.save('YourMusicLibrary' +'_var', std)
+	np.save('YourMusicLibrary' +'_x', inputData)
+	np.save('YourMusicLibrary' +'_y', outputData)
+	print("Done!")
